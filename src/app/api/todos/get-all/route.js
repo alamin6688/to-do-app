24dbdd6efd75baafe3acac7b09f4ca548392f790
alamin6688 <alamin.kzs06@gmail.com>
2024-10-27 10:58:30 +0600

    Modified

--- src/app/api/todos/get-all/route.js
+++ src/app/api/todos/get-all/route.js
@@ -1,12 +1,11 @@
 import { connectDB } from "@/lib/connectDB"
-import { NextResponse } from "next/server";
 
 export const GET = async () => {
     const db = await connectDB();
     const todoCollection = db.collection('to-do-list');
     try {
         const todos = await todoCollection.find().toArray();
-        return  NextResponse.json(todos);
+        return  Response.json(todos);
     } catch (error) {
         console.log(error
         )
